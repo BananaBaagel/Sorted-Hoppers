9e22258bd3ad953f945c9bba3b001cce53c51abb
Banana <66839366+BananaBaagel@users.noreply.github.com>
2023-09-11 06:37:11 -0400

    First Commit! (No function yet)

---
+++ src/main/java/com/banana/sortedhoppers/Logger.java
@@ -0,0 +1,24 @@
+package com.banana.sortedhoppers;
+
+import org.slf4j.LoggerFactory;
+
+public class Logger {
+    public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger("Sorted Hoppers");
+
+    public static void log(String message) {
+        LOGGER.info(message);
+    }
+
+    public static void info(String message) {
+        log(message);
+    }
+
+    public static void debug(String message) {
+        LOGGER.debug(message);
+    }
+
+    public static void error(String message) {
+        LOGGER.error(message);
+    }
+
+}
---
+++ src/main/java/com/banana/sortedhoppers/mixin/SortingHopperMixin.java
@@ -0,0 +1,10 @@
+package com.banana.sortedhoppers.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.entity.HopperBlockEntity;
+
+@Mixin(HopperBlockEntity.class)
+public class SortingHopperMixin {
+	
+}
